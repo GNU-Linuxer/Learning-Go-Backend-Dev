b99a0cde1bb3c528999af6ac3fe4c1fae4c2362c
David Xie <xdkdavid@gmail.com>
2022-02-05 05:17:54 -0800

    add control

--- program_control.go
+++ program_control.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"runtime"
+	"time"
 )
 
 // Go only has for-loop, no other program control flows
@@ -33,6 +35,35 @@ func main() {
 		pow(3, 2, 10),
 		pow(3, 3, 20),
 	)
+
+	// Switch statement for test value equality
+	// Switch cases evaluate cases from top to bottom, stopping when a case succeeds. (Go only run the selected case, not all the cases that follow, implies adding break to the end of case body)
+	fmt.Print("Go runs on ")
+	// switch within a block-scoped variable declaration
+	switch os := runtime.GOOS; os { // ; implies end of this statement, useful for writing multiple statement at a single line
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		fmt.Printf("%s. \n", os)
+	}
+	// fmt.Println(os) // undefined
+
+	// Switch with condition
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+
+	// defer modifier keyword
+	deferCall()
+	deferStack()
 }
 
 func sqrt(x float64) string {
@@ -52,4 +83,23 @@ func pow(x, n, lim float64) float64 {
 	}
 	// fmt.Println(v) // undefined
 	return lim
+}
+
+// A defer statement defers the execution of a function until the surrounding function returns.
+// The deferred call's arguments are evaluated immediately, but the function call is not executed until the surrounding function returns.
+func deferCall() {
+	defer fmt.Println("world")
+	// return
+	fmt.Println("hello")
+}
+
+// Deferred function calls are pushed onto a stack. When a function returns, its deferred calls are executed in last-in-first-out order.
+func deferStack() {
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
 }
