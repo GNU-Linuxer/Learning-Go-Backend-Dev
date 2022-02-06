b03ef4b6d46223f9ff242afb5ea39689c23a4005
David Xie <xdkdavid@gmail.com>
2022-02-05 22:23:25 -0800

    add routes

--- go-chi/simple-server.go
+++ go-chi/simple-server.go
@@ -11,7 +11,29 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "txt")
 		w.Write([]byte("welcome"))
 	})
+
+	// RESTy routes for "articles" resource
+	r.Route("/articles", func(r chi.Router) {
+		// GET /articles
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "txt")
+			w.Write([]byte("List Article Page"))
+		})
+		// GET /articles/01-16-2017
+		r.Get("/{month}-{day}-{year}", func(w http.ResponseWriter, r *http.Request) {
+			responseText := "Listing Articles on " + chi.URLParam(r, "month") + "-" + chi.URLParam(r, "day") + "-" + chi.URLParam(r, "year")
+			w.Write([]byte(responseText))
+		})
+		// POST /articles
+		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+			received := r.Body
+
+			w.Write([]byte(received))
+		})
+	})
+
 	http.ListenAndServe(":9000", r)
 }
